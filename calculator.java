d8576bc4ba8fc500987236cbbc86ef12ff880c9b
Pawan Agrawal <pavanagrawalofficial1@gmail.com>
2025-08-07 15:39:35 +0530

    added divide

---
+++ calculator.java
@@ -0,0 +1,10 @@
+
+public class calculator {
+    public double divide(double a, double b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Division by zero is not allowed.");
+        }
+        return a / b;
+    }
+
+}
