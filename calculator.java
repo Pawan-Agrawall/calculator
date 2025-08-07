aacb5ea2573b56247dff317788b93a380269cea1
dhruv-tiwari12 <tiwaridhruv151@gmail.com>
2025-08-07 15:59:05 +0530

    multiply

--- calculator.java
+++ calculator.java
@@ -1,3 +1,4 @@
+package calculator;
 
 public class calculator {
     public double divide(double a, double b) {
@@ -5,6 +6,10 @@ public class calculator {
             throw new IllegalArgumentException("Division by zero is not allowed.");
         }
         return a / b;
+    }
+
+        public static int multiply(int a,int b){
+        return a*b;
     }
 
 }
